797677341000aef50516155db0cdb67d67d05901
Adi Nugraha Setiadi <adi_nugraha_s@yahoo.com>
2013-05-21 04:43:29 +0700

    bisa klik event checkbox

--- ClientBox/src/clientbox/TaskListPanel.java
+++ ClientBox/src/clientbox/TaskListPanel.java
@@ -6,6 +6,8 @@ package clientbox;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.*;
 
@@ -28,6 +30,10 @@ public class TaskListPanel extends javax.swing.JPanel {
     private ArrayList<JLabel> description = new ArrayList<JLabel>();
     private ArrayList<JLabel> header = new ArrayList<JLabel>();
     
+    ArrayList<String> rowMessage = new ArrayList<String>();
+    ArrayList<String> columnMessage = new ArrayList<String>();
+    ArrayList<ArrayList<String>> dataMessage = new ArrayList<ArrayList<String>>();
+    
     String serverMessage;
     
     public TaskListPanel(String username) {
@@ -37,9 +43,8 @@ public class TaskListPanel extends javax.swing.JPanel {
         ClientBox b = new ClientBox();
         String m = b.run("localhost", 25000, ClientMessage.listMessage(username));
         
-        ArrayList<String> rowMessage = ParseMessage.parseMessage(m, ";");
-        ArrayList<String> columnMessage = new ArrayList<String>();
-        ArrayList<ArrayList<String>> dataMessage = new ArrayList<ArrayList<String>>();
+        rowMessage = ParseMessage.parseMessage(m, ";");
+        
         for(int i=0; i<rowMessage.size(); i++){
             columnMessage = ParseMessage.parseMessage(rowMessage.get(i), "#");
             dataMessage.add(columnMessage);
@@ -50,6 +55,7 @@ public class TaskListPanel extends javax.swing.JPanel {
             
             //for(int j=0; j<dataMessage.get(i).size(); j++){
                 //System.out.print(dataMessage.get(i).get(j)+":");
+                //row ke 0 id
                 if(dataMessage.get(i).get(4).equals("Completed")){
                     stat = true;
                 }
@@ -96,10 +102,10 @@ public class TaskListPanel extends javax.swing.JPanel {
         
         */
         for(int i = 0; i < dataMessage.size(); i++){
-            checkBox.setBounds(20, 30*i+50, 100, 30);
-            checkBox.setName("checkBox"+i);
-            checkBox.setVisible(true);
-            checkBoxPanel.add(checkBox);
+            //checkBox.setBounds(20, 30*i+50, 100, 30);
+            //checkBox.setName("checkBox"+i);
+            //checkBox.setVisible(true);
+            //checkBoxPanel.add(checkBox);
             
             c.get(i).setBounds(20,20*i,100,30);
             c.get(i).setVisible(true);
@@ -119,6 +125,62 @@ public class TaskListPanel extends javax.swing.JPanel {
         checkBoxPanel.setAutoscrolls(true);
         checkBoxPanel.setVisible(true);
         this.add(checkBoxPanel);
+        eventListenerCheckBox();
+        
+    }
+    public void setStatus(){
+        
+    }
+    
+    public boolean getStatus(JCheckBox a){
+        return a.isSelected();
+    }
+    
+    public void eventListenerCheckBox(){
+        JCheckBox temp = new JCheckBox();
+        
+        for(int i=0; i<c.size(); i++){
+            temp = c.get(i);
+            final String stats = "Status Check = "+c.get(i).isSelected();
+            System.out.println(stats);
+            final String id = dataMessage.get(i).get(0);
+            boolean status = getStatus(c.get(i));
+            
+            /*temp.addActionListener(new ActionListener(){
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    AbstractButton abstractButton = (AbstractButton)actionEvent.getSource();
+                    boolean selected = abstractButton.getModel().isSelected();
+                    abstractButton.setText(newLabel);
+                    System.out.println("Get Action Command "+e.getActionCommand());
+                    if(e.getActionCommand().equals("status"+id)){
+                        System.out.println(stats);
+                    }
+                }
+                
+            });
+            * 
+            */
+            
+            ActionListener actionListener = new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                AbstractButton abstractButton = (AbstractButton)actionEvent.getSource();
+                    boolean selected = abstractButton.getModel().isSelected();
+                    if(actionEvent.getActionCommand().equals("status"+id)){
+                        if(selected == true){
+                            //kirim pesan id x diubah menjadi status Completed
+                        }
+                        else{
+                            //kirim pesan id x diubah menjadi status Uncompleted
+                        }
+                    }
+                    System.out.println("Selected = "+selected);
+                }
+            };
+            
+            temp.addActionListener(actionListener);
+        }
     }
 
     /**
@@ -133,6 +195,7 @@ public class TaskListPanel extends javax.swing.JPanel {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        jCheckBox1 = new javax.swing.JCheckBox();
 
         jButton1.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
         jButton1.setText("Sync");
@@ -148,6 +211,13 @@ public class TaskListPanel extends javax.swing.JPanel {
         jLabel1.setFont(new java.awt.Font("Calibri", 0, 36)); // NOI18N
         jLabel1.setText("List Task");
 
+        jCheckBox1.setText("jCheckBox1");
+        jCheckBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -156,6 +226,8 @@ public class TaskListPanel extends javax.swing.JPanel {
                 .addContainerGap()
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 367, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jCheckBox1)
+                .addGap(18, 18, 18)
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -165,7 +237,8 @@ public class TaskListPanel extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jCheckBox1)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -187,9 +260,15 @@ public class TaskListPanel extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_jButton2ActionPerformed
 
+    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
+        // TODO add your handling code here:
+        System.out.println("test test test");
+    }//GEN-LAST:event_jCheckBox1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
