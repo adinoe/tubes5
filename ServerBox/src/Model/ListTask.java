1fc5b0d101513ca58ef0943587f46e2230151bff
AgungDLGS <agungdlgs@gmail.com>
2013-05-20 17:18:20 +0700

    server side
    list task

--- ServerBox/src/Model/ListTask.java
+++ ServerBox/src/Model/ListTask.java
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 package Model;
+import Entity.AssigneeTask;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,6 +11,7 @@ import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import Entity.Task;
+import java.util.ArrayList;
 import util.DbConfig;
 /**
  *
@@ -42,6 +44,78 @@ public class ListTask {
         closeDatabase();
       //  System.out.println("id_user ="+user);
         return user;
+    }
+     public static ArrayList<Task> getTaskAll(int id_user) throws ClassNotFoundException, SQLException
+    {
+        ArrayList<Task> creator = getTaskCreator(id_user);
+        ArrayList<Task> assignee = getTaskAssignee(id_user);
+        ArrayList<Task> temp = new ArrayList<Task>();
+        temp.addAll(creator);
+        temp.addAll(assignee);
+        openDatabase();
+        return temp;
+    }
+    public static ArrayList<Task> getTaskCreator(int id_user) throws ClassNotFoundException, SQLException
+    {
+        openDatabase();
+        ArrayList<Task> temp = new ArrayList<Task>();
+        String query = "SELECT * FROM task WHERE id_user="+id_user;
+        ResultSet rs = db.getStatement().executeQuery(query);
+        while(rs.next())
+        {
+            Task cmt=null;
+            cmt=new Task(rs.getInt("id_task"), id_user,rs.getString("title"),rs.getString("deadline"),rs.getString("description"),rs.getString("tags"),rs.getString("status"),rs.getInt("id_category"));
+            temp.add(cmt);
+            System.out.println("creator = "+cmt.getTitle());
+        }
+        closeDatabase();
+        return temp;
+    }
+    public static ArrayList<Task> getTaskAssignee(int id_user) throws ClassNotFoundException, SQLException
+    {
+        ArrayList<AssigneeTask> idTask = getIdAssignee(id_user);
+        ArrayList<Task> temp = new ArrayList<Task>();
+        openDatabase();
+        
+        ResultSet rs = null;
+        String query = "";
+        int tempIdTask  = 0;
+        int i = 0;
+        while (i<(idTask.size()))
+        {
+            tempIdTask = idTask.get(i).getIdTask();
+            query = "SELECT * FROM task WHERE id_task="+tempIdTask;
+            rs = db.getStatement().executeQuery(query);
+            while(rs.next())
+            {
+               Task cmt = null;
+               cmt=new Task(tempIdTask,id_user,rs.getString("title"),rs.getString("deadline"), rs.getString("description"), rs.getString("tags"),rs.getString("status"),rs.getInt("id_category"));
+               temp.add(cmt);
+               System.out.println("task = "+temp.get(i).getTitle());
+            }
+            i++;
+        }
+        closeDatabase();
+        return temp;
+    }
+    public static ArrayList<AssigneeTask> getIdAssignee(int id_user) throws ClassNotFoundException, SQLException
+    {
+        openDatabase();
+        String querty = "SELECT id_task FROM assignee_task WHERE id_user='"+id_user+"'";
+        ResultSet rs = db.getStatement().executeQuery(querty);
+        ArrayList<AssigneeTask> temp = new ArrayList<AssigneeTask>();
+        while(rs.next())
+        {
+            AssigneeTask cmt = null;
+            cmt=new AssigneeTask(rs.getInt("id_task"),id_user);
+            temp.add(cmt);
+        }
+        closeDatabase();
+        return temp;
+    }
+     public static void main(String[] args) throws ClassNotFoundException, SQLException
+    {
+        ArrayList<Task> sesuatucuk = getTaskAll(3);
     }
     
 }
